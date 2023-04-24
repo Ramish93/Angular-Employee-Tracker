f7b346fbd6128a6cbfc94f9c97c72c6a6b99ed41
ramishshah93 <ramishshah93@gmail.com>
2023-04-24 15:04:44 +0200

    getting all employees from db file through getEmployeeList func in employee.service file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-app';
 
-  constructor(private _dialog: MatDialog) {}
+  constructor(
+    private _dialog: MatDialog,
+    private _empService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
 
   addEditEmpForm() {
     this._dialog.open(EmpAddEditComponent);
+  }
+
+  getEmployeeList() {
+    this._empService.getEmployeeList().subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: () => {
+        console.log();
+      },
+    });
   }
 }
